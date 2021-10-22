683694c1a6c087cb1c6bb321a385f343481d8b0b
Antonio <a.brejo@campus.fct.unl.pt>
2021-10-22 22:14:00 +0100

    EletricFIeld

--- Project1/app.js
+++ Project1/app.js
@@ -12,8 +12,11 @@ let table_height;
 let verticesNum=0;
 const grid_spacing= 0.05;
 let charges=[];
+let vCharges=[];
 let chargesNum=0;
-const maxChargeNum = 20;
+const MAX_CHARGES = 20;
+const qCharge=1;
+const KE = 8.988*Math.pow(10,9);
 
 function setup(shaders)
 {
@@ -46,16 +49,18 @@ function setup(shaders)
 
     
     window.addEventListener("click", function(event){
-        if(chargesNum<maxChargeNum){
+        if(chargesNum<MAX_CHARGES){
             let x = (-1+(2*event.offsetX/canvas.width))*table_width/2;
             let y = (-1+(2*(canvas.height-event.offsetY)/canvas.height))*table_height/2;
             if(event.shiftKey){
                 charges.push(MV.vec2(x,y));
                 chargesNum++;
+                vCharges.push(-qCharge);
             }
             else{
                 charges.push(MV.vec2(x, y));
                 chargesNum++;
+                vCharges.push(qCharge);
             }
         }
         else {}
@@ -87,7 +92,20 @@ function rotate(time, charge){
     let x = charge[0];
     let y = charge[1];
     let radius = Math.sqrt(Math.pow(x,2)+Math.pow(y,2));
-    return [Math.cos(time/1000)*(radius/table_height),Math.sin(time/1000)*radius];
+    return [Math.cos(time/1000)*(radius),Math.sin(time/1000)*(radius)];
+}
+
+function calculateEF(vertice){
+    let eField=0;
+    let xVertice= vertice[0];
+    let yVertice = vertice[1];
+    for(let i = 0; i<chargesNum;i++){
+        let xCharge = charges[i][0];
+        let yCharge= charges[i][1];
+        let r = Math.sqrt(Math.pow(xVertice-xCharge,2)+Math.pow(yVertice-yCharge,2));
+        eField = eField + KE*vCharges[i]/(r*r);
+    }
+    return eField;
 }
 
 function animate(time)
@@ -102,7 +120,11 @@ function animate(time)
     gl.uniform1f(dx1,table_width);
     const dx2 = gl.getUniformLocation(program1,"utable_height");
     gl.uniform1f(dx2,table_height);
-    gl.drawArrays(gl.POINTS,0,verticesNum);
+    for(let i = 0; i <verticesNum;i++){
+        const eField = gl.getUniformLocation(program1,"eField");
+        gl.uniform1f(eField, calculateEF(vertices[i]));
+        gl.drawArrays(gl.POINTS,i,1);
+    }
 
     //Charges
     bufferInit2();
