c815e1bf62af49702a836f5d349b9b334f6d41b8
Antonio <a.brejo@campus.fct.unl.pt>
2021-10-23 21:32:07 +0100

    Field COmplete

--- Project1/app.js
+++ Project1/app.js
@@ -37,9 +37,9 @@ function setup(shaders)
 
     for(let x = -table_width/2+grid_spacing/2; x <= table_width/2+grid_spacing/2; x += grid_spacing) {
         for(let y = -table_height/2+grid_spacing/2; y <= table_height/2+grid_spacing/2; y += grid_spacing) {
-            vertices.push(MV.vec2(x, y));
+            vertices.push(MV.vec3(randFloat(x-grid_spacing/5,x+grid_spacing/5),randFloat(y-grid_spacing/5,y+grid_spacing/5),0.0));
             verticesNum++;
-            vertices.push(MV.vec2(x, y));
+            vertices.push(MV.vec3(x, y, 1.0));
             verticesNum++;
         }
     }
@@ -72,13 +72,16 @@ function setup(shaders)
     window.requestAnimationFrame(animate);
 
 }
+function randFloat(low,high){
+    return low + Math.random() * ( high - low );
+}
 
 function bufferInit(){
     const vBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER,vBuffer);
     gl.bufferData(gl.ARRAY_BUFFER,MV.flatten(vertices),gl.STATIC_DRAW);
     const vPosition = gl.getAttribLocation(program1,"vPosition");
-    gl.vertexAttribPointer(vPosition,2,gl.FLOAT,false,0,0);
+    gl.vertexAttribPointer(vPosition,3,gl.FLOAT,false,0,0);
     gl.enableVertexAttribArray(vPosition);
 }
 
@@ -101,19 +104,6 @@ function rotate(charge,value){
     return charge;
 }
 
-function calculateEF(vertice){
-    let eField=0;
-    let xVertice= vertice[0];
-    let yVertice = vertice[1];
-    for(let i = 0; i<chargesNum;i++){
-        let xCharge = charges[i][0];
-        let yCharge= charges[i][1];
-        let r = Math.sqrt(Math.pow(xVertice-xCharge,2)+Math.pow(yVertice-yCharge,2));
-        eField = eField + KE*vCharges[i]/(r*r);
-    }
-    return eField;
-}
-
 function animate(time)
 {
     window.requestAnimationFrame(animate);
@@ -126,11 +116,13 @@ function animate(time)
     gl.uniform1f(dx1,table_width);
     const dx2 = gl.getUniformLocation(program1,"utable_height");
     gl.uniform1f(dx2,table_height);
-    for(let i = 0; i <verticesNum;i++){
-        const eField = gl.getUniformLocation(program1,"eField");
-        gl.uniform1f(eField, calculateEF(vertices[i]));
-        gl.drawArrays(gl.LINES,i*2,2);
+    for(let i=0; i<chargesNum; i++) {
+        const uCharges = gl.getUniformLocation(program1, "uCharges[" + i + "]");
+        gl.uniform2fv(uCharges,MV.flatten(charges[i]));
+        const uVCharges = gl.getUniformLocation(program1, "uVCharges[" + i + "]");
+        gl.uniform1f(uVCharges,vCharges[i]);
     }
+    gl.drawArrays(gl.LINES,0,verticesNum);
 
     //Charges
     bufferInit2();
--- Project1/shaders/shader1.vert
+++ Project1/shaders/shader1.vert
@@ -2,7 +2,10 @@ uniform float utable_width;
 uniform float utable_height;
 attribute vec4 vPosition;
 varying vec4 fColor;
-uniform float eField;
+const int MAX_CHARGES=20;
+uniform vec2 uCharges[MAX_CHARGES];
+uniform float uVCharges[MAX_CHARGES];
+const float KE = 8.988; 
 
 #define TWOPI 6.28318530718
 
@@ -27,9 +30,40 @@ vec4 colorize(vec2 f)
     return vec4(angle_to_hue(a-TWOPI), 1.);
 }
 
+float getLength(vec2 vector){
+    return sqrt(vector.x*vector.x+vector.y*vector.y);
+}
+
+vec2 setLength(vec2 vector,float length){
+    vector = vector / vec2(sqrt(vector.x*vector.x+vector.y*vector.y),sqrt(vector.x*vector.x+vector.y*vector.y));
+    vector = vector * vec2(length,length);
+    return vector;
+}
+vec2 calculateEfield(vec4 vPosition, vec2 chargeP,float vCharge){
+    vec2 eField = vec2(0.0,0.0);
+    eField.x= vPosition.x-chargeP.x;
+    eField.y= vPosition.y-chargeP.y;
+    float fieldV = KE * vCharge/(pow(vPosition.x-chargeP.x,2.0) + pow(vPosition.y-chargeP.y,2.0))/500.0;
+    if(fieldV>0.25) fieldV = 0.25;
+    else if(fieldV<-0.25) fieldV = -0.25;
+    eField = setLength(eField,fieldV);
+    return eField;
+}
+
 void main()
 {
     gl_PointSize=4.0;
-    gl_Position = vPosition / vec4(utable_width/2.0, utable_height/2.0,1.0,1.0);
+    if(vPosition.z==0.0){
+        gl_Position = vPosition/ vec4(utable_width/2.0, utable_height/2.0,1.0,1.0);
+    }
+    else{
+        vec2 eField= vec2(0.0,0.0);
+        for(int i = 0; i<MAX_CHARGES;i++){
+            eField = eField + calculateEfield(vPosition, uCharges[i],uVCharges[i]);
+        }
+        if(getLength(eField)>0.25) eField = setLength(eField,0.25);
+        gl_Position = (vPosition + vec4(eField,0.0,0.0)) / vec4(utable_width/2.0, utable_height/2.0,1.0,1.0);
+        gl_Position.z=0.0;
+    }
     fColor=colorize(vec2(gl_Position.x,gl_Position.y));
 }
